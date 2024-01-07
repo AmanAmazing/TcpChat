9963d3561faac04fb3c2912c23be58c272db1176
Amanpreet Gill <aman@Amanpreets-Mac-mini.local>
2024-01-07 12:24:21 +0000

    Updated the server to accept multiple clients and broadcast the messages to all clients

--- server/main.go
+++ server/main.go
@@ -5,9 +5,21 @@ import (
 	"log"
 	"net"
 	"os"
-
+	"sync"
 )
 
+type Client struct {
+	conn net.Conn
+	ch   chan<- string
+}
+
+var (
+	clients    = make(map[Client]bool)
+	register   = make(chan Client)
+	unregister = make(chan Client)
+	broadcast  = make(chan string)
+	mutex      = &sync.Mutex{}
+)
 
 func main() {
 	port := ":8000"
@@ -17,29 +29,54 @@ func main() {
 		os.Exit(1)
 	}
 	defer listner.Close()
-	fmt.Println("Server is listening on port: ",port)
+	fmt.Println("Server is listening on port: ", port)
+    go handleChannels()
 	for {
 		conn, err := listner.Accept()
 		if err != nil {
 			fmt.Println(err)
-            continue
+			continue
 		}
-		go HandleConn(conn)
-
+        ch := make(chan string)
+        client := Client{conn,ch}
+        register <- client
+		go HandleConn(client)
 	}
 }
 
-func HandleConn(conn net.Conn) {
-    defer conn.Close()
-	fmt.Println("Following IP address connected: ", conn.RemoteAddr())
-    buffer := make([]byte,1024)
+func HandleConn(client Client){
+	defer client.conn.Close()
+	fmt.Println("Following IP address connected: ", client.conn.RemoteAddr())
+	buffer := make([]byte, 1024)
+	for {
+		n, err := client.conn.Read(buffer)
+		if err != nil {
+            unregister <- client
+            break
+		}
+		message := string(buffer[:n])
+        broadcast <- message
+	}
+}
+
+func handleChannels(){
     for {
-        n, err := conn.Read(buffer)
-        if err !=nil {
-            fmt.Println("Error reading from connection:",err)
-            return
+        select{
+        case client := <-register:
+            mutex.Lock()
+            clients[client] = true
+            mutex.Unlock()
+        case client := <-unregister:
+            mutex.Lock()
+            if _,ok := clients[client]; ok {
+                close(client.ch)
+                delete(clients,client)
+            }
+            mutex.Unlock()
+        case message := <- broadcast: 
+            for client := range clients{
+                client.ch <- message
+            }
         }
-        message := string(buffer[:n])
-        fmt.Println("Received:",message)
     }
 }
